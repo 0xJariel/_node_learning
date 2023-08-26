b9dbc98ef115c30b0130c043be7dc3a7b3094368
J <0xjariel@protonmail.com>
2023-08-25 23:06:44 -0600

    routes and urls

--- server.js
+++ server.js
@@ -1,9 +1,58 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  //   set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  //   res.write("<p>hello jay</p>");
+  //   res.end();
+
+  // send html file
+  //   fs.readFile("./views/index.html", (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //       res.end();
+  //     } else {
+  //       //   res.write(data);
+  //       res.end(data);
+  //     }
+  //   });
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  //   send html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("now listening");
+  console.log("now listening on port 3000");
 });
