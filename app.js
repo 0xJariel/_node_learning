a37b6011f42fe992ef39ff0a34914a5f8fcc07d4
J <0xjariel@protonmail.com>
2023-08-28 22:43:28 -0600

    test assignemtn complete

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const EventEmitter = require("events");
 
+const emitter = new EventEmitter();
+
+emitter.emit("messageLogged");
 const app = express();
 
 app.listen(3000);
